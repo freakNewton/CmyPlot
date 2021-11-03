13c200d6fd069a76c45c6c2b9a1b064fa00abbf6
nsshah5 <nsshah5@ncsu.edu>
2021-11-03 19:49:27 -0400

    minor change

--- src/plotting/index.py
+++ src/plotting/index.py
@@ -6,10 +6,10 @@ import argparse
 from src.plotting.environment.settings import APP_PORT, APP_DEBUG, DEV_TOOLS_PROPS_CHECK
 
 # initializes all callbacks
-import utils.routes
-import pages.upload.upload_callbacks
-import pages.table.table_callbacks
-import pages.graph.graph_callbacks
+import src.plotting.utils.routes
+import src.plotting.pages.upload.upload_callbacks
+import src.plotting.pages.table.table_callbacks
+import src.plotting.pages.graph.graph_callbacks
 
 # initialize services
 server = app.server
