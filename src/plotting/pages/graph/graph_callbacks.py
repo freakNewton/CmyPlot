37f6c48c56ab09af08d4428ac6d18955a9f9bbf2
SimranBosmiya <bosamiyasimran@gmail.com>
2021-11-03 16:49:48 -0400

    sharing image via email working

--- src/plotting/pages/graph/graph_callbacks.py
+++ src/plotting/pages/graph/graph_callbacks.py
@@ -6,8 +6,10 @@ from dash import callback_context
 # from dash import dcc
 from PIL import Image as PImage
 import email as email
-from plotly.io import to_image
+import plotly.io as pio
+from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 import pandas as pd
 import plotly.express as px
 import smtplib
@@ -114,9 +116,9 @@ def create_figure(data, att_values, label_values, height):
         figure: plotly.graph_objects.Figure
             Created graph object
     """
-
     if not func.validate_store_data(data) or all(i is None for i in att_values):
         raise PreventUpdate
+    # print(type(data), att_values, label_values, height)
 
     # zip keys with values for easy dictionary access
     attributes = dict(zip(go.attributes, att_values))
@@ -147,13 +149,14 @@ def create_figure(data, att_values, label_values, height):
         labels=graph_labels,
         height=height
     )
+
     # print(type(figure), figure.to_json())
     # dcc.Store(id="graphstore", data=figure.to_json(), storage_type='session')
     # img = figure.to_image(format="png")
     # if not os.path.exists("src/plotting/assets/images"):
     #     os.mkdir("src/plotting/assets/images/")
     # img.write_image("src/plotting/assets/images/graph.png")
-    joblib.dump(figure, "src/plotting/assets/images/fig.pkl")
+    # joblib.dump(figure, "src/plotting/assets/images/fig.pkl")
     return figure
 
 
@@ -161,32 +164,76 @@ def create_figure(data, att_values, label_values, height):
     Output('share-modal', 'is_open'),
     [Input('share-button', 'n_clicks'), Input('send-button', 'n_clicks')],
     #  Input(graph.graph_id.format('graphstore'), 'graph_id')
-    State("share-modal", "is_open")
+    [State("share-modal", "is_open"), 
+     State({"type": go.att_drop, "index": ALL}, "value"),
+     State({"type": go.label_input, "index": ALL}, "value"),
+     State(store_id, 'data'),
+     State(go.graph_height, 'value')]
 )
-def share_graph(n1, n2, is_open):
+def share_graph(n1, n2, is_open, att_drop, label_input, data, height):
     # print(is_open)
     if(is_open is True):
+        # print(type(data), att_drop, label_input, height)
         message = "Hello"
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
             # Identify ourselves with the mail server we are using.
+            
+            # fig = create_figure(data, att_drop, label_input, height)
+            # print(type(figure), figure)
+            attributes = dict(zip(go.attributes, att_drop))
+            labels = dict(zip(go.labels, label_input))
+
+            # prep data
+            df = pd.DataFrame(data['df'])
+
+            # Set the x and y axis labels
+            graph_labels = {}
+
+            x_att = attributes[go.x_att]
+            x_lab = labels[go.x_lab]
+            graph_labels[x_att] = x_lab if (x_att and x_lab) else x_att
+
+            y_att = attributes[go.y_att]
+            y_lab = labels[go.y_lab]
+            graph_labels[y_att] = y_lab if (y_att and y_lab) else y_att
+
+            # create the scatter plot
+            fig = px.scatter(
+                df,
+                x=x_att,
+                y=y_att,
+                size=attributes[go.size],
+                color=attributes[go.color],
+                title=labels[go.title],
+                labels=graph_labels,
+                height=height
+            )
+            # if not os.path.exists("src/plotting/assets/images"):
+            #     os.mkdir("src/plotting/assets/images/")
+            pio.write_image(fig, "src/plotting/assets/images/graph.png")
+            # return True
+            # temp = joblib.load("src/plotting/assets/images/fig.pkl")
+            # -----------------------
+            # plot_img = to_image(fig)
+            # -----------------------
+            # print(type(plot_img))
+            # print(type(plot_img), plot_img)
+            # plot_img = PImage.open("src/plotting/assets/images/graph.png")
+            with open("src/plotting/assets/images/graph.png", 'rb') as f:
+                img_data = f.read()
+            message = MIMEMultipart()
+            image = MIMEImage(img_data, name="graph.png")
+            message.attach(image)
+            # print("here1")
             smtp.ehlo()
             # Encrypt our connection
             smtp.starttls()
             # Reidentify our connection as encrypted with the mail server
             smtp.ehlo()
             smtp.login(sender_email, sender_pwd)
-            # print(type(figure), figure)
-
-            # -----------------------
-            plot_img = to_image(joblib.load("src/plotting/assets/images/fig.pkl"))
-            # -----------------------
-
-            # print(type(plot_img), plot_img)
-            # plot_img = PImage.open("images/graph.png")
-            message = MIMEImage(plot_img)
-            message.attach(plot_img, subtype='.png',
-                           _encoder=email.encoders.encode_base64)
-            smtp.sendmail(sender_email, receiver_email, message)
+            smtp.sendmail(sender_email, receiver_email, message.as_string())
+            smtp.quit()
+            # print("here2")
     if(n1 or n2):
         return not is_open
     return is_open
