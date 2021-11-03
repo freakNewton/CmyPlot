6399bcb683ce25333cb6ac7b60e0e028048917c3
SimranBosmiya <bosamiyasimran@gmail.com>
2021-11-03 16:48:48 -0400

    turned off hot reload to stop refreshing page on writing file in assets

--- src/plotting/index.py
+++ src/plotting/index.py
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     app.run_server(
         host=args.host,
         port=APP_PORT,
-        debug=APP_DEBUG,
-        dev_tools_props_check=DEV_TOOLS_PROPS_CHECK
+        debug=True,
+        dev_tools_props_check=DEV_TOOLS_PROPS_CHECK,
+        dev_tools_hot_reload=False
     )
